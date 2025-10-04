e150372ec5e092705c54a8f3cba384076e60e79a
akshayyad <akshayyad09@gmail.com>
2025-10-03 17:17:58 -0700

    Implemented 2 Player Mode

--- main.py
+++ main.py
@@ -61,14 +61,7 @@ def print_state(state):
     for i in range(6):
         print(state[i])
 
-empty = create_empty_game_state()
 
-#print_state(empty)
-#print()
-#new_state = add_piece("B", 5, empty)
-#print_state(new_state)
-
-print(check_state_win(empty))
     
 
 # ("E", "E", "E", "E", "E", "E", "E"), 
@@ -77,3 +70,63 @@ print(check_state_win(empty))
 # ("E", "E", "E", "E", "E", "E", "E"), 
 # ("E", "E", "E", "E", "E", "E", "E"), 
 # ("E", "E", "E", "E", "E", "E", "E")
+
+def single_player():
+    pass
+
+def two_player(first_color, second_color):
+    gameState = create_empty_game_state()
+    print_state(gameState)
+    print()
+    while (1 == 1):
+        firstMove = int(input(f"Enter the Column to add a piece for {first_color} (1-7 from Left to Right): "))
+        print()
+        gameState = add_piece(first_color[0], firstMove-1, gameState)
+        print_state(gameState)
+        print()
+        if check_state_win(gameState) != 0:
+            print(f"Congradulations for the Win Team {first_color}!\n")
+            break
+
+        secondMove = int(input(f"Enter the Column to add a piece for {second_color} (1-7 from Left to Right): "))
+        print()
+        gameState = add_piece(second_color[0], secondMove-1, gameState)
+        print_state(gameState)
+        print()
+        if check_state_win(gameState) != 0:
+            print(f"Congradulations for the Win Team {second_color}!\n")
+            break
+
+
+def game_controller():
+    gamemode = int(input("Input 1 if you want to play against the computer, or 2 for Two-Player Mode: "))
+    if gamemode == 1:
+        single_player()
+    if gamemode == 2:
+        choice = input("Input the color that will go first: ")
+        secondChoice = ''
+        if choice == "B":
+            choice = "Blue"
+            secondChoice = 'Red'
+        elif choice == "R":
+            choice = "Red"
+            secondChoice = 'Blue'
+        two_player(choice, secondChoice)
+
+
+def main():
+    empty = create_empty_game_state()
+    # print_state(empty)
+    # print()
+    # new_state = add_piece("B", 5, empty)
+    # print_state(new_state)
+
+    # print(check_state_win(empty))
+
+    print("Welcome to Connect 4")
+    game_controller()
+    print("Thank you for playing")
+    
+
+if __name__ == "__main__":
+    main()
